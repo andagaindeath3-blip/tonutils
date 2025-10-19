d97f18d327928b90b20ee265fdd4c3418f2c730a
ness <shonness98@gmail.com>
2025-10-19 17:51:19 +0500

    RIntroduce `ClientLike` generic alias for unified client types.

--- tonutils/clients/__init__.py
+++ tonutils/clients/__init__.py
@@ -1,10 +1,21 @@
+import typing as t
+
 from .liteserver import LiteserverClient
 from .quicknode import QuicknodeClient
 from .tatum import TatumClient
 from .tonapi import TonapiClient
 from .toncenter import ToncenterClient
 
+ClientLike = t.Union[
+    LiteserverClient,
+    QuicknodeClient,
+    TatumClient,
+    ToncenterClient,
+    TonapiClient,
+]
+
 __all__ = [
+    "ClientLike",
     "LiteserverClient",
     "QuicknodeClient",
     "TatumClient",
